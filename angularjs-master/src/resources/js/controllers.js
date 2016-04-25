3fe6d9019fbf253dfa39e280f3bb2dc677080788
ralucavacaru <raluca_vacaru@yahoo.com>
2016-04-25 21:11:17 +0100

    Added continuity logic & 2D heatmap

--- angularjs-master/src/resources/js/controllers.js
+++ angularjs-master/src/resources/js/controllers.js
@@ -1,45 +1,52 @@
 var DocMuncher = angular.module('DocMuncher', ['ngRoute']);
 
-DocMuncher.value('currentProject', '');
-DocMuncher.value('currentDocument', '');
+DocMuncher.factory('UserService', function() {
+  return {
+    currentProject: '',
+    currentCluster: '',
+    currentTab: '',
+    tabs: [
+        {
+            name: 'Home',
+            color: '#30afa4',
+            link: '#/',
+        },
+        {
+            name: 'Project Overview',
+            color: '#009fda',
+            link: '#/projectOverview',
+        },
+        {
+            name: 'Cluster Overview',
+            color: '#6a1ccd',
+            link: '#/clusterOverview',
+        },
+        {
+            name: 'Document Overview',
+            color: '#be0f12',
+            link: '#/documentOverview',
+        },
+        {
+            name: 'Saved Documents',
+            color: '#aac327',
+            link: '#/savedDocuments',
+        },
+    ]
+  };
+});
 
-DocMuncher.controller('HeaderCtrl', function ($scope) {
+DocMuncher.controller('HeaderCtrl', function ($scope, UserService) {
     $scope.activeTab = 'Home';
-  	$scope.tabs = [
-        {
-        	name: 'Home',
-        	color: '#30afa4',
-        	link: '#/',
-        },
-        {
-        	name: 'Project Overview',
-        	color: '#009fda',
-        	link: '#/projectOverview',
-        },
-        {
-        	name: 'Cluster Overview',
-        	color: '#6a1ccd',
-        	link: '#/clusterOverview',
-        },
-        {
-        	name: 'Document Overview',
-        	color: '#be0f12',
-        	link: '#/documentOverview',
-        },
-        {
-        	name: 'Saved Documents',
-        	color: '#aac327',
-        	link: '#/savedDocuments',
-        },
-    ];
+  	$scope.tabs = UserService.tabs;
 
     $scope.selectedTab = $scope.tabs[0];
     $scope.setMasterTab = function(tab) {
         $scope.selectedTab = tab;
+        UserService.currentTab = tab;
     }
 
     $scope.isSelectedTab = function(tab) {
-        return $scope.selectedTab === tab;
+        return UserService.currentTab === tab;
     }
 });
 
@@ -74,7 +81,7 @@ DocMuncher.controller('PageCtrl', function (/* $scope, $location, $http */) {
 	console.log("Page Controller reporting for duty.");
 });
 
-DocMuncher.controller('HomeCtrl', function ($scope, currentProject) {
+DocMuncher.controller('HomeCtrl', function ($scope, UserService) {
     $scope.projects = [
         {
             id: 1,
@@ -132,19 +139,23 @@ DocMuncher.controller('HomeCtrl', function ($scope, currentProject) {
         name: 'New Project',
     }
 
-    $scope.selected = {
-        id: 1,
-        name: 'Overview',
-        link: '#',
-        active: 'active',
-        ingested: 32432432,
-        riskyClusters: 324,
-        riskyDocuments: 342342,
+    if (UserService.currentProject === '') {
+        $scope.selected = {
+            name: 'Overview',
+            link: '#',
+            ingested: 432432,
+            riskyClusters: 34,
+            riskyDocuments: 4322,
+        }
     }
+    else {
+        $scope.selected = UserService.currentProject;
+    }
+
 
     $scope.setMaster = function(project) {
         $scope.selected = project;
-        currentProject = project;
+        UserService.currentProject = project;
     }
 
     $scope.isSelected = function(project) {
@@ -199,106 +210,209 @@ DocMuncher.controller('SavedCtrl', function ($scope) {
     }
 });
 
-DocMuncher.controller('ProjectCtrl', function ($scope, currentProject) {
+DocMuncher.controller('ProjectCtrl', function ($scope, UserService, $location) {
    $(function () {
-    Highcharts.setOptions({
-     colors: ['#eb534e', '#4db853', '#ebae54'],
-     chart: {
-            style: {
-                fontFamily: '"Roboto Condensed", sans-serif',
-                fontSize: '14px'
-            }
-        }
-    });
-    // Create the chart
-    $('#pie-chart-container').highcharts({
-        chart: {
-            type: 'pie',
-            backgroundColor: null
-        },
-        title: {
-            text: currentProject.name
-        },
-        subtitle: {
-            text: 'Click the slices to view sub-clusters'
-        },
-        plotOptions: {
-            series: {
-                dataLabels: {
-                    enabled: true,
-                    format: '{point.name}: {point.y:.1f}%'
+        Highcharts.setOptions({
+         colors: ['#eb534e', '#4db853', '#ebae54'],
+         chart: {
+                style: {
+                    fontFamily: '"Roboto Condensed", sans-serif',
+                    fontSize: '14px'
                 }
             }
-        },
+        });
+        // Create the chart
+        $('#pie-chart-container').highcharts({
+            chart: {
+                type: 'pie',
+                backgroundColor: null
+            },
+            title: {
+                text: ''
+            },
+            subtitle: {
+                text: 'Click the slices to view sub-clusters'
+            },
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.name}: {point.y:.1f}%',
+                    },
+                    allowPointSelect: true,
+                    point: {
+                        events: {
+                            select: function() {                                
+                                UserService.currentCluster = this;
+                                UserService.currentTab = UserService.tabs[2];
+                                $scope.$apply(function() { $location.path("/clusterOverview"); });
+                            }
+                        }  
+                    }
+                },
+            },
 
-        tooltip: {
-            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-        },
-        series: [{
-            name: 'Clusters',
-            colorByPoint: true,
-            data: [{
-                name: 'Risky',
-                y: 56.33,
-                drilldown: 'Risky'
-            }, {
-                name: 'Risk-Free',
-                y: 24.03,
-                drilldown: 'Risk-Free'
-            }, {
-                name: 'Borderline',
-                y: 10.38,
-                drilldown: 'Borderline'
-            }]
-        }],
-        drilldown: {
+            tooltip: {
+                headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+            },
             series: [{
-                name: 'Risky',
-                id: 'Risky',
-                colorByPoint: false,
-                data: [
-                    ['Cluster 1', 24.13],
-                    ['Cluster 2', 17.2],
-                    ['Cluster 3', 8.11],
-                    ['Cluster 4', 5.33],
-                    ['Cluster 5', 1.06],
-                    ['Cluster 6', 0.5]
-                ]
-            }, {
-                name: 'Risk-Free',
-                id: 'Risk-Free',
-                colorByPoint: false,
-                data: [
-                    ['Cluster 1', 5],
-                    ['Cluster 2', 4.32],
-                    ['Cluster 3', 3.68],
-                    ['Cluster 4', 2.96],
-                    ['Cluster 5', 2.53],
-                    ['Cluster 6', 1.45],
-                    ['Cluster 7', 1.24],
-                    ['Cluster 8', 0.85],
-                    ['Cluster 9', 1.24],
-                    ['Cluster 10', 0.85]
-                ]
-            }, {
-                name: 'Boderline',
-                id: 'Borderline',
-                colorByPoint: false,
-                data: [
-                    ['Cluster 1', 2.76],
-                    ['Cluster 2', 2.32],
-                    ['Cluster 3', 2.31],
-                    ['Cluster 4', 1.27],
-                    ['Cluster 5', 1.02],
-                    ['Cluster 6', 0.33],
-                    ['Cluster 7', 0.22],
-                    ['Cluster 8', 0.15]
-                ]
+                name: 'Project Overview',
+                colorByPoint: true,
+                data: [{
+                    name: 'Risky',
+                    y: 56.33,
+                    drilldown: 'Risky'
+                }, {
+                    name: 'Risk-Free',
+                    y: 24.03,
+                    drilldown: 'Risk-Free'
+                }, {
+                    name: 'Borderline',
+                    y: 10.38,
+                    drilldown: 'Borderline'
+                }]
+            }],
+            drilldown: {
+                series: [{
+                    name: 'Risky',
+                    id: 'Risky',
+                    colorByPoint: false,
+                    data: [
+                        ['Cluster 1', 24.13],
+                        ['Cluster 2', 17.2],
+                        ['Cluster 3', 8.11],
+                        ['Cluster 4', 5.33],
+                        ['Cluster 5', 1.06],
+                        ['Cluster 6', 0.5]
+                    ]
+                }, {
+                    name: 'Risk-Free',
+                    id: 'Risk-Free',
+                    colorByPoint: false,
+                    data: [
+                        ['Cluster 1', 5],
+                        ['Cluster 2', 4.32],
+                        ['Cluster 3', 3.68],
+                        ['Cluster 4', 2.96],
+                        ['Cluster 5', 2.53],
+                        ['Cluster 6', 1.45],
+                        ['Cluster 7', 1.24],
+                        ['Cluster 8', 0.85],
+                        ['Cluster 9', 1.24],
+                        ['Cluster 10', 0.85]
+                    ]
+                }, {
+                    name: 'Boderline',
+                    id: 'Borderline',
+                    colorByPoint: false,
+                    data: [
+                        ['Cluster 1', 2.76],
+                        ['Cluster 2', 2.32],
+                        ['Cluster 3', 2.31],
+                        ['Cluster 4', 1.27],
+                        ['Cluster 5', 1.02],
+                        ['Cluster 6', 0.33],
+                        ['Cluster 7', 0.22],
+                        ['Cluster 8', 0.15]
+                    ]
+                }]
+            }
+        });
+    });
+
+    $(function () {
+        $('#heat-map-container').highcharts({
+            chart: {
+                type: 'heatmap',
+                marginTop: 40,
+                marginBottom: 80,
+                plotBorderWidth: 0,
+                backgroundColor: null
+            },
+
+
+            title: {
+                text: ''
+            },
+
+            xAxis: {
+                categories: ['World Wide Imp.', 'Widget Care', 'Coho', 'Litware'],
+                gridLineColor: '#FFFFFF',
+                labels: {
+                    style: {
+                        fontSize:'15px'
+                    }
+                }
+            },
+
+            yAxis: {
+                categories: ['NDA', 'HR Policies', 'Supply of Goods', 'IP Rights', 'Unknown Cluster'],
+                title: null,
+                gridLineColor: '#FFFFFF',
+                labels: {
+                    style: {
+                        fontSize:'15px'
+                    }
+                }
+            },
+
+            colorAxis: {
+                min: 0,
+                stops: [
+                    [0, '#4db853'],
+                    [0.2, '#4db853'],
+                        [0.3, '#ebae54'],
+                    [0.5, '#ebae54'],
+                    [0.7, '#ebae54'],
+                    [0.8, '#eb534e']
+                ],
+            },
+
+            plotOptions: {
+                series: {
+                    allowPointSelect: true,
+                    point: {
+                        events: {
+                            select: function() {   
+                                UserService.currentCluster = this;
+                                UserService.currentTab = UserService.tabs[2];
+                                $scope.$apply(function() { $location.path("/clusterOverview"); });
+                            }
+                        }  
+                    }
+                },
+            },
+
+            legend: {
+                align: 'right',
+                layout: 'vertical',
+                margin: 0,
+                verticalAlign: 'top',
+                y: 25,
+                symbolHeight: 280,
+            },
+
+            tooltip: {
+                formatter: function () {
+                    return '</b>' + this.point.value + '%</b> risk index';
+                }
+            },
+
+            series: [{
+                name: '',
+                borderWidth: 1,
+                data: [[0, 0, 10], [0, 1, 19], [0, 2, 68], [0, 3, 24], [0, 4, 4], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 17], [1, 4, 38], [2, 0, 35], [2, 1, 15], [2, 2, 13], [2, 3, 64], [2, 4, 78], [3, 0, 72], [3, 1, 32], [3, 2, 84], [3, 3, 19], [3, 4, 11]],
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000'
+                }
             }]
-        }
+
+        });
     });
-    });
+
+    $scope.currentProject = UserService.currentProject;
 
     $scope.selected = 1;
 
@@ -309,8 +423,16 @@ DocMuncher.controller('ProjectCtrl', function ($scope, currentProject) {
     $scope.isSelected = function(aux) {
         return $scope.selected === aux;
     }
+
+    $scope.setMasterCluster = function(aux) {
+        UserService.currentCluster = aux;
+    }
 });
 
+
+DocMuncher.controller('ClusterCtrl', function ($scope, UserService) {
+    $scope.currentCluster = UserService.currentCluster;
+});
 // Use for embedding pdf files.
 // Syntax: <embed embed-src="..." type='application/pdf'>
 DocMuncher.directive('embedSrc', function () {
