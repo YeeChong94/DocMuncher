4ff115006fb31441e6b75615300cd76eb7c4a970
ralucavacaru <raluca_vacaru@yahoo.com>
2016-04-26 02:27:58 +0100

    More refactoring

--- angularjs-master/src/resources/js/controllers.js
+++ angularjs-master/src/resources/js/controllers.js
@@ -46,80 +46,132 @@ DocMuncher.factory('UserService', function() {
             name: 'Document 2',
             risk: 85,
             link: '../resources/pdf/2.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: 'Etiam tellus ligula, tempus vel suscipit efficitur, pretium eleifend nunc. Mauris posuere vel tellus vel venenatis. Aenean elementum vel tortor id tempus. Ut id malesuada turpis, sed facilisis ligula.',
         },
         {
             name: 'Document 3',
             risk: 56,
             link: '../resources/pdf/3.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: 'Mauris facilisis sagittis odio eget elementum. Suspendisse ut semper lacus. Sed erat nulla, bibendum vel mauris nec, malesuada varius odio. In bibendum ullamcorper turpis vitae condimentum. Vivamus sed pharetra orci. ',
         },
         {
             name: 'Document 4',
             risk: 76,
             link: '../resources/pdf/4.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: 'Nam sodales enim ut eros feugiat, in pretium turpis condimentum. Donec sodales ante quis mauris cursus dignissim. Vestibulum tellus quam, commodo porta est nec, dignissim mollis sem. Sed in fermentum purus.',
         },
         {
             name: 'Document 5',
             risk: 16,
             link: '../resources/pdf/5.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: '',
         },
         {
             name: 'Document 6',
             risk: 73,
             link: '../resources/pdf/6.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: '',
         },
         {
             name: 'Document 7',
             risk: 13,
             link: '../resources/pdf/7.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: 'Nam hendrerit sem non nibh auctor eleifend. Donec ex tellus, accumsan vitae euismod sed, tincidunt vel magna. Nunc at orci ac nunc ornare dignissim id eleifend leo. Etiam a est suscipit, semper arcu at, sodales nisi.',
         },
         {
             name: 'Document 8',
             risk: 14,
             link: '../resources/pdf/8.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: 'Duis eget ex id sapien mollis accumsan. Etiam semper magna sed nisl tempor, nec pretium tellus ornare. Integer quis suscipit felis, et dapibus purus. Praesent et hendrerit odio. Integer venenatis ut nibh nec accumsan.',
         },
         {
             name: 'Document 9',
             risk: 32,
             link: '../resources/pdf/9.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: '',
         },
         {
             name: 'Document 10',
             risk: 58,
             link: '../resources/pdf/10.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: 'Suspendisse sed blandit neque, pharetra sodales quam. Sed sed libero sit amet est egestas cursus. Nam consectetur orci sed eros feugiat, ac imperdiet mi placerat. In non dui nec nulla hendrerit scelerisque et vel lorem.',
         },
         {
             name: 'Document 11',
             risk: 64,
             link: '../resources/pdf/11.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: '',
         },
         {
             name: 'Document 12',
             risk: 70,
             link: '../resources/pdf/12.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: 'Aliquam porttitor tortor sit amet libero auctor tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed quam sem, dignissim dictum dictum non, rhoncus ut ante. In odio neque, bibendum nec hendrerit at, lacinia sed lacus. Nulla suscipit arcu nec commodo efficitur. Praesent at malesuada nulla. Suspendisse nec tortor lobortis, consequat purus varius, vehicula nunc. Etiam interdum ullamcorper porttitor.',
         },
         {
             name: 'Document 13',
             risk: 42,
             link: '../resources/pdf/13.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: 'Pellentesque quam elit, blandit non bibendum in, scelerisque vitae ipsum. Vestibulum eu ipsum eu massa imperdiet tincidunt ornare quis ex. Aliquam eu mi facilisis nunc placerat placerat vitae eu sem. ',
         },
         {
             name: 'Document 14',
             risk: 58,
             link: '../resources/pdf/14.pdf',
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis.',
+            notes: '',
         },
+    ],
+    projects: [
+        {
+            id: 1,
+            name: 'Specific performance clauses in national legislation of European States',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis. Curabitur mauris lacus, pulvinar ac lectus quis, euismod lobortis libero. Morbi sit amet mollis velit, at gravida elit. Donec ullamcorper est id elit gravida pulvinar. Sed suscipit lorem justo, et scelerisque urna blandit nec.',
+            link: '#',
+            active: 'active',
+            ingested: 43232,
+            riskyClusters: 34,
+            riskyDocuments: 653,
+        },
+        {
+            id: 2,
+            name: 'Infringment procedures against national states on a specific directive',
+            description: 'Pellentesque felis nunc, dictum vel bibendum non, convallis a sem. Phasellus nibh lectus, consectetur vel mi vitae, congue volutpat nibh. Vivamus turpis massa, dictum porta ante eu, molestie elementum orci. Maecenas sed tortor vel magna consequat cursus. Vivamus dictum, urna non sagittis tincidunt, nibh lectus porttitor leo, et finibus tellus ex eget arcu.',
+            link: '#',
+            active: 'active',
+            ingested: 5636,
+            riskyClusters: 65,
+            riskyDocuments: 324,
+        },
+        {
+            id: 3,
+            name: 'Conditions precedent on Free Hill loan agreement',
+            description: 'Nullam id nibh nisl. Nam iaculis rhoncus felis, eu gravida dui tincidunt eu. Maecenas at mollis tortor. Etiam pellentesque magna at ultricies tempor. Phasellus non commodo sapien, a sodales risus.',
+            link: '#',
+            active: 'active',
+            ingested: 545,
+            riskyClusters: 542,
+            riskyDocuments: 55555,
+        },
+        {
+            id: 4,
+            name: 'National legislation on unfair contract terms',
+            description: 'Quisque sed dui eu erat congue maximus. Sed non vulputate lacus. Nullam et ante at lorem gravida cursus ac ut velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed id finibus elit. Duis ultrices maximus purus, nec posuere massa feugiat at.',
+            link: '#',
+            active: 'active',
+            ingested: 4522,
+            riskyClusters: 5,
+            riskyDocuments: 54,
+        },
+        {
+            id: 5,
+            name: 'Definition and verification of securities in a loan agreement',
+            description: 'Phasellus ac augue tellus. Duis scelerisque, mauris vitae hendrerit semper, dolor ligula ullamcorper ipsum, eu luctus justo arcu eleifend dui.',
+            link: '#',
+            active: 'active',
+            ingested: 432432,
+            riskyClusters: 34,
+            riskyDocuments: 4322,
+        }
     ]
   };
 });
@@ -171,58 +223,8 @@ DocMuncher.controller('PageCtrl', function (/* $scope, $location, $http */) {
 });
 
 DocMuncher.controller('HomeCtrl', function ($scope, UserService) {
-    $scope.projects = [
-        {
-            id: 1,
-            name: 'Specific performance clauses in national legislation of European States',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis. Curabitur mauris lacus, pulvinar ac lectus quis, euismod lobortis libero. Morbi sit amet mollis velit, at gravida elit. Donec ullamcorper est id elit gravida pulvinar. Sed suscipit lorem justo, et scelerisque urna blandit nec.',
-            link: '#',
-            active: 'active',
-            ingested: 43232,
-            riskyClusters: 34,
-            riskyDocuments: 653,
-        },
-        {
-            id: 2,
-            name: 'Infringment procedures against national states on a specific directive',
-            description: 'Pellentesque felis nunc, dictum vel bibendum non, convallis a sem. Phasellus nibh lectus, consectetur vel mi vitae, congue volutpat nibh. Vivamus turpis massa, dictum porta ante eu, molestie elementum orci. Maecenas sed tortor vel magna consequat cursus. Vivamus dictum, urna non sagittis tincidunt, nibh lectus porttitor leo, et finibus tellus ex eget arcu.',
-            link: '#',
-            active: 'active',
-            ingested: 5636,
-            riskyClusters: 65,
-            riskyDocuments: 324,
-        },
-        {
-            id: 3,
-            name: 'Conditions precedent on Free Hill loan agreement',
-            description: 'Nullam id nibh nisl. Nam iaculis rhoncus felis, eu gravida dui tincidunt eu. Maecenas at mollis tortor. Etiam pellentesque magna at ultricies tempor. Phasellus non commodo sapien, a sodales risus.',
-            link: '#',
-            active: 'active',
-            ingested: 545,
-            riskyClusters: 542,
-            riskyDocuments: 55555,
-        },
-        {
-            id: 4,
-            name: 'National legislation on unfair contract terms',
-            description: 'Quisque sed dui eu erat congue maximus. Sed non vulputate lacus. Nullam et ante at lorem gravida cursus ac ut velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed id finibus elit. Duis ultrices maximus purus, nec posuere massa feugiat at.',
-            link: '#',
-            active: 'active',
-            ingested: 4522,
-            riskyClusters: 5,
-            riskyDocuments: 54,
-        },
-        {
-            id: 5,
-            name: 'Definition and verification of securities in a loan agreement',
-            description: 'Phasellus ac augue tellus. Duis scelerisque, mauris vitae hendrerit semper, dolor ligula ullamcorper ipsum, eu luctus justo arcu eleifend dui.',
-            link: '#',
-            active: 'active',
-            ingested: 432432,
-            riskyClusters: 34,
-            riskyDocuments: 4322,
-        }
-    ];
+    UserService.currentTab = UserService.tabs[0];
+    $scope.projects = UserService.projects;
 
     $scope.new = {
         name: 'New Project',
@@ -252,36 +254,14 @@ DocMuncher.controller('HomeCtrl', function ($scope, UserService) {
     }
 });
 
-DocMuncher.controller('SavedCtrl', function ($scope) {
-    $scope.documents = [
-        {
-            id: 1,
-            name: 'Document 1',
-            link: '../resources/pdf/1.pdf',
-            risk: 87,
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet. Ut nec elit dictum, maximus dui non, facilisis turpis. Curabitur mauris lacus, pulvinar ac lectus quis, euismod lobortis libero. Morbi sit amet mollis velit, at gravida elit. Donec ullamcorper est id elit gravida pulvinar. Sed suscipit lorem justo, et scelerisque urna blandit nec.',
-        },
-        {
-            id: 2,
-            name: 'Document 2',
-            link: '../resources/pdf/2.pdf',
-            risk: 43,
-            notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a aliquet mauris, sed bibendum dui. Etiam posuere risus ac purus ornare imperdiet.',
-        },
-        {
-            id: 3,
-            name: 'Document 3',
-            link: '../resources/pdf/3.pdf',
-            risk: 3,
-            notes: 'Ut nec elit dictum, maximus dui non, facilisis turpis. Curabitur mauris lacus, pulvinar ac lectus quis, euismod lobortis libero. Morbi sit amet mollis velit, at gravida elit.',
-        },
-        {
-            id: 4,
-            name: 'Document 4',
-            link: '../resources/pdf/4.pdf',
-            risk: 78,
-            notes: 'Nullam id nibh nisl. Nam iaculis rhoncus felis, eu gravida dui tincidunt eu. Maecenas at mollis tortor. Etiam pellentesque magna at ultricies tempor. ',
-        },
+DocMuncher.controller('SavedCtrl', function ($scope, UserService) {
+    UserService.currentTab = UserService.tabs[4];
+    $scope.documents = [UserService.documents[4], 
+                        UserService.documents[6], 
+                        UserService.documents[7], 
+                        UserService.documents[9], 
+                        UserService.documents[11], 
+                        UserService.documents[3]
     ];
 
     $scope.new = {
@@ -300,6 +280,7 @@ DocMuncher.controller('SavedCtrl', function ($scope) {
 });
 
 DocMuncher.controller('ProjectCtrl', function ($scope, UserService, $location) {
+    UserService.currentTab = UserService.tabs[1];
    $(function () {
         Highcharts.setOptions({
          colors: ['#eb534e', '#4db853', '#ebae54'],
@@ -320,13 +301,14 @@ DocMuncher.controller('ProjectCtrl', function ($scope, UserService, $location) {
                 text: ''
             },
             subtitle: {
-                text: 'Click the slices to view sub-clusters'
+                text: 'Click the slices to view division by clusters'
             },
             plotOptions: {
                 series: {
                     dataLabels: {
                         enabled: true,
                         format: '{point.name}: {point.y:.1f}%',
+                        fontSize: '14px',
                     },
                     allowPointSelect: true,
                     point: {
@@ -419,6 +401,7 @@ DocMuncher.controller('ProjectCtrl', function ($scope, UserService, $location) {
             xAxis: {
                 categories: ['World Wide Imp.', 'Widget Care', 'Coho', 'Litware'],
                 gridLineColor: '#FFFFFF',
+                minorGridLineColor: '#FFFFFF',
                 labels: {
                     style: {
                         fontSize:'15px'
@@ -430,6 +413,7 @@ DocMuncher.controller('ProjectCtrl', function ($scope, UserService, $location) {
                 categories: ['NDA', 'HR Policies', 'Supply of Goods', 'IP Rights', 'Unknown Cluster'],
                 title: null,
                 gridLineColor: '#FFFFFF',
+                minorGridLineColor: '#FFFFFF',
                 labels: {
                     style: {
                         fontSize:'15px'
@@ -442,7 +426,7 @@ DocMuncher.controller('ProjectCtrl', function ($scope, UserService, $location) {
                 stops: [
                     [0, '#4db853'],
                     [0.2, '#4db853'],
-                        [0.3, '#ebae54'],
+                    [0.3, '#ebae54'],
                     [0.5, '#ebae54'],
                     [0.7, '#ebae54'],
                     [0.8, '#eb534e']
@@ -513,6 +497,7 @@ DocMuncher.controller('ProjectCtrl', function ($scope, UserService, $location) {
 });
 
 DocMuncher.controller('ClusterCtrl', function ($scope, UserService, $location) {
+    UserService.currentTab = UserService.tabs[2];
     $scope.currentCluster = UserService.currentCluster;
     $scope.documents = UserService.documents;
 
@@ -531,6 +516,7 @@ DocMuncher.controller('ClusterCtrl', function ($scope, UserService, $location) {
 });
 
 DocMuncher.controller('DocCtrl', function ($scope, UserService, $location) {
+    UserService.currentTab = UserService.tabs[3];
     $scope.currentDoc = UserService.currentDoc;
     console.log($scope.currentDoc.link);
 });
